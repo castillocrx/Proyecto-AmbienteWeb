a0fbf8a0fb4666d5ef9e24442ab182d7156f464d
castillocrx <105761899+castillocrx@users.noreply.github.com>
2023-10-24 21:08:58 -0600

    mostrar productos y script de bd

--- mainProductos.php
+++ mainProductos.php
@@ -1,29 +1,33 @@
 <?php
 include_once "include/template/header.php";
 ?>
+
 <div class="bienv-pos">
     <h1 class="bienvenido">Las mejores hamburguesas.</h1>
 </div>
-<main class="contenedor">
-            <?php
-            require_once "DL/producto.php";
-            $elSql = "select id, nombre, descripcion, imagen, precio from productos";
-            $myArray = getArray($elSql);
-            if (!empty($myArray)) {
-                foreach ($myArray as $value) {
-                    echo "<tr>";
-                    echo "<td>" . $value['nombre'] . "</td>";
-                    echo "<td>{$value['descripcion']}</td>";
-                    echo "<td><img src='{$value['imagen']}' alt='Imagen del producto' height='100'></td>";
-                    echo "<td>{$value['precio']}</td>";
-                    echo "</tr>";
-                }
-            } else {
-                echo "<tr>No hay registros de alumnos</tr>";
-            }
-            ?>
 
+<main class="contenedor">
+    <div class="productos">
+        <?php
+        require_once "DL/producto.php";
+        $elSql = "select id, nombre, descripcion, imagen, precio from productos";
+        $myArray = getArray($elSql);
+        if (!empty($myArray)) {
+            foreach ($myArray as $value) {
+                echo "<div class='producto'>";
+                echo "<h2>{$value['nombre']}</h2>";
+                echo "<p>{$value['descripcion']}</p>";
+                echo "<img src='{$value['imagen']}' alt='Imagen del producto' height='100'>";
+                echo "<p>Precio: {$value['precio']}</p>";
+                echo "</div>";
+            }
+        } else {
+            echo "<p>No hay productos disponibles.</p>";
+        }
+        ?>
+    </div>
 </main>
+
 <?php
 require_once "include/template/footer.php";
 ?>
