afc8b0e4ec3e23208adcd3bf0e0fea2d186b7fd8
BryanMJ04 <109931931+BryanMJ04@users.noreply.github.com>
2023-11-13 20:53:55 -0600

    descuentos

---
+++ descuentos.php
@@ -0,0 +1,27 @@
+<?php
+include_once "include/template/header.php";
+?>
+
+<link rel="stylesheet" href="css/descuentos.css">
+
+
+<body>
+    <main>
+        <div class="contenido">
+            <div class="cont-imagen">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            <img class="logo" src="img/bbq.jpg" alt="Logo de tienda">
+            </div>
+        </div>
+    </main>
+</body>
+
+<?php
+require_once "include/template/footer.php";
+?>
