6c0b83255e51a4149551ccf053a89a8185974e75
William040902CR <105571295+William040902CR@users.noreply.github.com>
2023-11-21 22:08:44 -0600

    Update creacion.sql
    Se añadieron más tablas

--- DB/creacion.sql
+++ DB/creacion.sql
@@ -6,3 +6,29 @@ CREATE TABLE productos (
     `Precio` DOUBLE NOT NULL,
     PRIMARY KEY (`Id`)
 ) ENGINE = InnoDB;
+
+
+CREATE TABLE usuario (
+    `Id_usuario` INT NOT NULL AUTO_INCREMENT,
+    `Correo` VARCHAR(150) NOT NULL,
+    `Contraseña` VARCHAR(150) NOT NULL,
+    `Nombre` VARCHAR(100) NOT NULL,
+    `Apellido` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`Id_usuario`)
+) ENGINE = InnoDB;
+
+CREATE TABLE factura (
+    `Id_factura` INT NOT NULL AUTO_INCREMENT,
+    `Id_usuario` INT NOT NULL ,
+    `Id_producto` INT NOT NULL ,
+    `Fecha` DATE NOT NULL,
+    PRIMARY KEY (`Id_factura`)
+) ENGINE = InnoDB;
+
+CREATE TABLE descuento (
+    `Id_descuento` INT NOT NULL AUTO_INCREMENT,
+    `Id_factura` INT NOT NULL ,
+    `Id_producto` INT NOT NULL ,
+    `Porcentaje` FLOAT NOT NULL,
+    PRIMARY KEY (`Id_descuento`)
+) ENGINE = InnoDB;
