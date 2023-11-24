5d26293266046ba6b85b24d8868222647ae33fdb
BryanMJ04 <109931931+BryanMJ04@users.noreply.github.com>
2023-11-23 18:36:42 -0600

    arreglos

--- DB/creacion.sql
+++ DB/creacion.sql
@@ -1,3 +1,11 @@
+CREATE TABLE productos (
+    `Id` INT NOT NULL AUTO_INCREMENT,
+    `Nombre` VARCHAR(100) NOT NULL,
+    `Descripción` TEXT NOT NULL,
+    `Imagen` VARCHAR(255) NOT NULL,
+    `Precio` DOUBLE NOT NULL,
+    PRIMARY KEY (`Id`)
+) ENGINE = InnoDB;
 
 CREATE TABLE clientes (
     `idCliente` INT NOT NULL AUTO_INCREMENT,
